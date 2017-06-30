336f23d07aafe1bfa19d09f8a935f264db0dcacf
Kat Gifford <katgiff2007@gmail.com>
2017-06-30 08:36:55 -0700

    add db tables

--- db/migrate/20170630152456_create_products.rb
+++ db/migrate/20170630152456_create_products.rb
@@ -1,6 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.column :name, :string
+      t.column :style, :string
+      t.column :cost, :decimal, :precision => 8, :scale => 2
+      t.column :origin, :string
+      t.column :brand, :string
+
+      t.timestamps
     end
   end
 end
