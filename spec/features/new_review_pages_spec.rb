12340eb8cb441aaa2ea0cfbcefa3811504849334
Kat Gifford <katgiff2007@gmail.com>
2017-06-30 17:06:43 -0700

    add 4th int spec

--- spec/features/new_review_pages_spec.rb
+++ spec/features/new_review_pages_spec.rb
@@ -6,7 +6,6 @@ DatabaseCleaner.strategy = :truncation
 describe 'visit product path, route to product details' do
   it 'navigates from product path to sentence details' do
     product1 = Product.create(:name => 'product', :cost => 25.0, :origin => "Boston, MA", :brand => "RAD", :style => "Bottoms", :url => "http://mindlessones.com/wp-content/uploads/2012/04/HermesMoebius04.jpg")
-    review1 = product1.reviews.create(:author => 'Maddie Sparkle', :content => 'The fit is perfect', :rating => 4)
     visit root_path
     click_link 'SHOP'
     click_link 'product'
