577f91cb0c9d3a3cdbd8286a06cae0c412c60e14
Kat Gifford <katgiff2007@gmail.com>
2017-07-05 20:11:20 -0700

    adjust faker-lorem ipsum with char limit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Review.destroy_all
   product = Product.create!(
     name: Faker::Coffee.blend_name,
     cost: Faker::Number.decimal(2),
-    origin: Faker::Coffee.origin,
+    origin: Faker::Address.country,
     brand: Faker::Superhero.descriptor,
     style: Faker::Coffee.variety,
     url: Faker::Avatar.image
@@ -13,7 +13,7 @@ Review.destroy_all
   5.times do |i|
     review = product.reviews.create!(
         author: Faker::TwinPeaks.character,
-        content: Faker::Coffee.notes,
+        content: Faker::Lorem.characters(75),
         rating: Faker::Number.between(1, 5)
         )
   end
