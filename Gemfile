e3945d23bcadceb63c8dfda243f481ca96028e89
Kat Gifford <katgiff2007@gmail.com>
2017-07-05 21:55:43 -0700

    add css

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass'
+gem 'flexbox-sass'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,30 @@
 Product.destroy_all
 Review.destroy_all
 
-50.times do |i|
+45.times do |i|
   product = Product.create!(
     name: Faker::Coffee.blend_name,
     cost: Faker::Number.decimal(2),
     origin: Faker::Address.country,
+    brand: Faker::Superhero.descriptor,
+    style: Faker::Coffee.variety,
+    url: Faker::Avatar.image
+    )
+  5.times do |i|
+    review = product.reviews.create!(
+        author: Faker::TwinPeaks.character,
+        content: Faker::Lorem.characters(75),
+        rating: Faker::Number.between(1, 5)
+        )
+  end
+end
+
+
+5.times do |i|
+  product = Product.create!(
+    name: Faker::Coffee.blend_name,
+    cost: Faker::Number.decimal(2),
+    origin: "USA",
     brand: Faker::Superhero.descriptor,
     style: Faker::Coffee.variety,
     url: Faker::Avatar.image
