06a73eb8ac15d6b0733c718fc235e45ea8a873ab
Kat Gifford <katgiff2007@gmail.com>
2017-07-06 21:32:22 -0700

    add spec for validating review length

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,15 @@
 class Review < ActiveRecord::Base
   validates :author, :content, :rating, :presence => true
+
   validates_length_of :content, maximum: 250, message: "we appreciate your feedback, butcharacter maximum allowed has been reached"
+
   validates_length_of :content, minimum: 50, message: "is too short (minimum is 50 characters)"
+
   validates_numericality_of :rating, on: :create
-  validates_numericality_of :rating, :less_than_or_equal_to => 5, :greater_than_or_equal_to => 0
+
+  validates_numericality_of :rating, :less_than_or_equal_to => 5, message: "must be less than or equal to 5"
+
+  validates_numericality_of :rating, :greater_than => 0, message: "must be greater than 0"
 
   belongs_to :product
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,5 @@ describe Review do
   it { should validate_presence_of :rating}
   it { should belong_to :product }
   it { should validate_length_of(:content).is_at_least(50) }
-  # it { should validate_numericality_of(:rating).only_integer.is_less_than(5).is_greater_than(0) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5).is_greater_than(0) }
 end
